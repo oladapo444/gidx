3da6b82c17deef1a67ffa0a11c0c60c6577f2e8c
oladapo444 <151373552+oladapo444@users.noreply.github.com>
2024-02-01 08:30:21 +0100

    Add files via upload

---
+++ Divisible.py
@@ -0,0 +1,5 @@
+num = int(input('Enter a number:'))
+if num % 3 == 0 or num % 5 == 0:
+    print('the number is divisible by 3')
+else:
+    print('is divisible by 5')
---
+++ EvenAndOddNumber.py
@@ -0,0 +1,6 @@
+
+num = int(input('Enter any number to test if is even or odd: '))
+if num % 2 == 0 and num % 3 == 0: 
+    print('Even')
+else:
+    print('is a odd number')
