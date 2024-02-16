9b0b588921dd1c1cfbc0e097852a600191fb5bbd
oladapo444 <151373552+oladapo444@users.noreply.github.com>
2024-02-16 13:17:44 +0100

    Add files via upload

---
+++ Largest.py
@@ -0,0 +1,8 @@
+Number1 = int(input("Enter the first number:"))
+Number2 = int(input("Enter the second:"))
+
+for i in range(1):
+    if  Number1 > Number2:
+        print("the Largest number is:", Number1)
+    elif Number2 > Number1:
+        print("the Largest number:", Number2)
